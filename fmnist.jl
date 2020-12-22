e9387f7a451278fff1d78337dcd14256caf90de1
Yenson <yenson.lau@gmail.com>
2020-12-22 00:29:44 -0500

    Fashion MNIST exercise.

--- fmnist.jl
+++ fmnist.jl
@@ -1,4 +1,8 @@
+using Flux
+using Flux: onehotbatch, onecold, @epochs
+using Flux: Data.DataLoader, Losses.logitcrossentropy
 using MLDatasets: FashionMNIST
+using Parameters: @with_kw
 
 Xtrain, ytrain = FashionMNIST.traindata()
 Xtest, ytest = FashionMNIST.testdata()
