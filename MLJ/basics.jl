c3cb34b26d12e14ceb4754d44ae51aecc301312a
Yenson <yenson.lau@gmail.com>
2021-02-15 01:27:08 -0500

    Basics: CategoricalArrays and ScientificTypes.

--- MLJ/basics.jl
+++ MLJ/basics.jl
@@ -1,3 +1,4 @@
+## Split-Apply-Combine
 using RDatasets, DataFrames
 using Statistics
 
@@ -21,3 +22,56 @@ combine(groupby(iris, :Species), :PetalLength => mean => :MPL,
 
 # an also broadcast multiple columns to one function
 combine(gdf, names(iris, Not(:Species)) .=> std)
+
+
+
+## Categorical vectors
+using CategoricalArrays
+
+# a column of categorical feature values
+v = categorical(["AA", "BB", "CC", "AA", "BB", "CC"]);
+v_categories = levels(v)  # returns unique values in order
+
+# categoricals can also be ordered, e.g. default lexicographic order
+v = categorical([1, 2, 3, 1, 2, 3, 1, 2, 3], ordered=true);
+
+# or specify order manually
+v = categorical(["high", "med", "low", "high", "med", "low"], ordered=true);
+new_order = ["low", "med", "high"];
+levels!(v, new_order);
+
+# levels of categorical vectors disregard missing values
+v = categorical(["AA", "BB", missing, "AA", "BB", "CC"]);
+levels(v)
+
+
+
+## Scientific types: Continuous, Count(able), OrderedFactor
+using MLJScientificTypes
+
+boston = dataset("MASS", "Boston");
+sch = schema(boston);
+
+# Note unique(boston.Chas) is just [0,1]... should be OrderedFactor
+boston2 = coerce(boston, :Chas=>OrderedFactor, :Rad=>OrderedFactor);
+println(eltype(boston2.Chas))     # CategoricalArrays.CategoricalValue{Int64,UInt32}
+println(elscitype(boston2.Chas))  # OrderedFactor{2}
+println();
+
+# Strings default to Textual scitype, can coerce or drop
+feature = ["AA", "BB", "AA", "AA", "BB"]
+println(elscitype(feature));
+
+feature2 = coerce(feature, Multiclass)
+println(elscitype(feature2));
+println();
+
+# We can also coerce entire datatypes
+data = select(boston, [:Rad, :Tax])
+data2 = coerce(data, Count => Continuous)
+schema(data2)
+
+# We can also coerce using autotypes
+#   :few_to_finite, :discrete_to_continuous, :string_to_multiclass
+boston3 = coerce(boston, autotype(boston, :few_to_finite))
+schema(boston3)
